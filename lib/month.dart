05d7f790e1eff535bb988b5a19fef56262567db2
zhangjiji <loszhang@gmailc.om>
2024-02-22 14:52:08 +0800

    修改

--- lib/month.dart
+++ lib/month.dart
@@ -1,5 +1,7 @@
+import 'package:calendar/color_constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 import 'component/NearlyTask.dart';
@@ -12,20 +14,25 @@ class Month extends StatefulWidget {
 }
 
 class _MonthState extends State<Month> {
-
   bool week = true;
 
   bool checked = false;
 
   double weekFontSize = 15;
-  TextStyle weekStyle = TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.grey);
-  TextStyle todayWeekStyle = TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black);
-  TextStyle weekTodayDayStyle = TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.black);
+  TextStyle weekStyle =
+      TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.grey);
+  TextStyle todayWeekStyle =
+      TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black);
+  TextStyle weekTodayDayStyle =
+      TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.black);
 
-  TextStyle dayStyle = TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.grey);
-  TextStyle todayDayStyle = TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.white);
+  TextStyle dayStyle =
+      TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.grey);
+  TextStyle todayDayStyle =
+      TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.white);
 
-  TextStyle nealyTaskStyle = TextStyle(fontSize: 10, fontWeight: FontWeight.bold, color: Colors.black);
+  TextStyle nealyTaskStyle =
+      TextStyle(fontSize: 10, fontWeight: FontWeight.bold, color: Colors.black);
 
   DateTime now = DateTime.now();
   int today = DateTime.now().day;
@@ -33,7 +40,6 @@ class _MonthState extends State<Month> {
   int daysInMonth = 30; // 假设当前月份有29天
   int startWeekday = DateTime.now().weekday; // 当月第一天是周几
   List<String> dayLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 
   @override
   void initState() {
@@ -45,30 +51,34 @@ class _MonthState extends State<Month> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Padding(
-          padding: EdgeInsets.only(top: 10, left: 3),
-          child: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.keyboard_arrow_left_outlined,
-              size: 50,
-            ),
+          leading: Padding(
+        padding: EdgeInsets.only(top: 10, left: 3),
+        child: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.keyboard_arrow_left_outlined,
+            size: 50,
           ),
-        )
-      ),
+        ),
+      )),
       body: Column(
         children: [
-          SizedBox(height: 10,),
-          month(),
-          SizedBox(height: 10,),
-          week ? weekView() : Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.7,
-            child: monthView(),
-
+          SizedBox(
+            height: 10,
           ),
+          month(),
+          SizedBox(
+            height: 10,
+          ),
+          week
+              ? weekView()
+              : SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  child: monthTableView(),
+                ),
         ],
       ),
     );
@@ -90,7 +100,10 @@ class _MonthState extends State<Month> {
             GestureDetector(
               child: Text(
                 'W',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500, color: week ? Colors.black : Colors.grey),
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: week ? Colors.black : Colors.grey),
               ),
               onTap: () {
                 setState(() {
@@ -98,13 +111,24 @@ class _MonthState extends State<Month> {
                 });
               },
             ),
-            SizedBox(width: 5,),
-            Text('/', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500, color: Colors.grey)),
-            SizedBox(width: 5,),
+            SizedBox(
+              width: 5,
+            ),
+            Text('/',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey)),
+            SizedBox(
+              width: 5,
+            ),
             GestureDetector(
               child: Text(
                 'M',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500, color: week ? Colors.grey : Colors.black),
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: week ? Colors.grey : Colors.black),
               ),
               onTap: () {
                 setState(() {
@@ -118,36 +142,201 @@ class _MonthState extends State<Month> {
     );
   }
 
+  List<TableRow> buildWeekDays() {
+    bool isToday;
+    return List.generate(2, (index) {
+      return TableRow(
+        children: dayLabels.map((label) {
+          if (index == 0) {
+            isToday = DateTime.now().weekday == dayLabels.indexOf(label) + 1;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(label,
+                  textAlign: TextAlign.center,
+                  style: isToday ? todayWeekStyle : weekStyle),
+            );
+          } else {
+            isToday = DateTime.now().weekday == dayLabels.indexOf(label) + 1;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  '${getWeekDates()[dayLabels.indexOf(label)].split('-')[2]}',
+                  textAlign: TextAlign.center,
+                  style: isToday ? weekTodayDayStyle : dayStyle),
+            );
+          }
+        }).toList(),
+      );
+    });
+  }
+
+  Widget weekTableView() {
+    return Table(
+      columnWidths: const {
+        0: FlexColumnWidth(),
+        1: FlexColumnWidth(),
+        2: FlexColumnWidth(),
+        3: FlexColumnWidth(),
+        4: FlexColumnWidth(),
+        5: FlexColumnWidth(),
+        6: FlexColumnWidth(),
+      },
+      children: [
+        ...buildWeekDays(),
+        const TableRow(children: [
+          Divider(thickness: 1, indent: 5, endIndent: 5),
+          Divider(thickness: 1, indent: 5, endIndent: 5),
+          Divider(thickness: 1, indent: 5, endIndent: 5),
+          Divider(thickness: 1, indent: 5, endIndent: 5),
+          Divider(thickness: 1, indent: 5, endIndent: 5),
+          Divider(thickness: 1, indent: 5, endIndent: 5),
+          Divider(thickness: 1, indent: 5, endIndent: 5),
+        ]),
+        // Add your NearlyTask widgets here as TableRow
+        // Each NearlyTask should be wrapped with a TableCell and a Padding if needed
+        const TableRow(
+          children: [
+            TableCell(
+                verticalAlignment: TableCellVerticalAlignment.middle,
+                child: SizedBox(height: 10)),
+            // Replace with your NearlyTask Widget
+            TableCell(child: SizedBox()),
+            // Empty cells for gaps
+            TableCell(child: SizedBox()),
+            TableCell(child: SizedBox()),
+            TableCell(child: SizedBox()),
+            TableCell(child: SizedBox()),
+            TableCell(child: SizedBox()),
+          ],
+        ),
+      ],
+    );
+  }
+
   Widget weekView() {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: dayLabels.map((label) {
-            bool isToday = DateTime.now().weekday == dayLabels.indexOf(label) + 1;
-            return Text(label, style: isToday ? todayWeekStyle : weekStyle);
-          }).toList(),
+        weekTableView(),
+        SizedBox(
+          height: 10,
         ),
-        const Divider(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: getWeekDates().map((date) {
-            bool isToday = DateTime.now().weekday == getWeekDates().indexOf(date) + 1;
-            return Text(date.split('-')[2], style: isToday ? weekTodayDayStyle : dayStyle);
-          }).toList(),
-        ),
-        SizedBox(height: 10,),
         NearlyTask(
           title: 'Cleaning Room',
           time: '08:00 - 10:00 /Personal',
           initiallyChecked: false,
         ),
-        SizedBox(height: 10,),
+        SizedBox(
+          height: 10,
+        ),
         NearlyTask(
           title: 'Cooking',
           time: '12:00 - 13:00 /Personal',
           initiallyChecked: false,
         ),
+      ],
+    );
+  }
+
+  Widget monthTableView() {
+    // 计算需要的网格行数
+    int totalSlots = daysInMonth + startWeekday - 1;
+    int numRows = (totalSlots / 7).ceil();
+
+    // 构建表格行
+    List<TableRow> buildMonthDays() {
+      List<TableRow> rows = [];
+      for (int i = 0; i < numRows; i++) {
+        List<Widget> dayWidgets = [];
+        for (int j = 0; j < 7; j++) {
+          int index = i * 7 + j;
+          if (index < startWeekday - 1 ||
+              index >= daysInMonth + startWeekday - 1) {
+            // 如果是前空位或者后空位
+            dayWidgets.add(Container()); // 显示空白格子
+          } else {
+            // 格子内的日期
+            int dayNumber = index - startWeekday + 2;
+            bool isToday = dayNumber == today;
+            dayWidgets.add(Container(
+              color: isToday ? ColorConstant.crackBlack : null,
+              width: MediaQuery.of(context).size.width / 7,
+              child: SizedBox(
+                height: 100,
+                child: Column(
+                  children: [
+                    Text(
+                      '$dayNumber',
+                      style: isToday ? todayDayStyle : dayStyle,
+                    ),
+                    // 示例待办事项，根据实际需求显示
+                    dayNumber % 6 == 0 ? Column(
+                      children: [
+                        Container(
+                          height: 10,
+                          color: Colors.lightBlue,
+                        ),
+                        Container(
+                          height: 10,
+                          color: Colors.orange,
+                        )
+                      ],
+                    ) : Container(),
+                  ],
+                ),
+              ),
+            ));
+          }
+        }
+        rows.add(TableRow(children: dayWidgets));
+      }
+      return rows;
+    }
+
+    // 构建星期标签的行
+    TableRow buildWeekDays() {
+      return TableRow(
+        children: dayLabels.map((label) {
+          bool isToday = DateTime.now().weekday == dayLabels.indexOf(label) + 1;
+          return Container(
+            width: MediaQuery.of(context).size.width / 7, // 指定宽度，使得与日期宽度一致
+            height: 20, // 指定高度，使得与日期高度一致
+            alignment: Alignment.center,
+            child: Text(
+              label,
+              style: isToday ? todayWeekStyle : weekStyle,
+              textAlign: TextAlign.center,
+            ),
+          );
+        }).toList(),
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Display the weekday texts
+        Table(
+          columnWidths:
+              { for (var index in List.generate(7, (index) => index)) index : FixedColumnWidth(MediaQuery.of(context).size.width / 7) },
+          // 设定宽度，你可以设置为你想要的尺寸
+          children: [
+            buildWeekDays(),
+            const TableRow(
+              children: [
+                Divider(),
+                Divider(),
+                Divider(),
+                Divider(),
+                Divider(),
+                Divider(),
+                Divider(),
+              ]
+            ),
+
+            ...buildMonthDays(),
+          ],
+        ),
+
       ],
     );
   }
@@ -160,7 +349,8 @@ class _MonthState extends State<Month> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: dayLabels.map((label) {
-            bool isToday = DateTime.now().weekday == dayLabels.indexOf(label) + 1;
+            bool isToday =
+                DateTime.now().weekday == dayLabels.indexOf(label) + 1;
             return Text(label, style: isToday ? todayWeekStyle : weekStyle);
           }).toList(),
         ),
@@ -168,7 +358,7 @@ class _MonthState extends State<Month> {
         // 使用GridView.builder生成每个月的日视图
         Expanded(
           child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 7, // 一周七天，所以crossAxisCount设置为7
               childAspectRatio: 1 / 2, // 子项目的高宽比
               crossAxisSpacing: 0,
@@ -221,7 +411,8 @@ class _MonthState extends State<Month> {
 
     for (int i = 0; i < 7; i++) {
       DateTime dayOfWeek = startOfWeek.add(Duration(days: i));
-      weekDates.add("${dayOfWeek.year}-${dayOfWeek.month.toString().padLeft(2, '0')}-${dayOfWeek.day.toString().padLeft(2, '0')}");
+      weekDates.add(
+          "${dayOfWeek.year}-${dayOfWeek.month.toString().padLeft(2, '0')}-${dayOfWeek.day.toString().padLeft(2, '0')}");
     }
 
     return weekDates;
@@ -236,5 +427,4 @@ class _MonthState extends State<Month> {
     DateTime lastDayOfMonth = DateTime(year, month + 1, 0);
     return lastDayOfMonth.day;
   }
-
 }
